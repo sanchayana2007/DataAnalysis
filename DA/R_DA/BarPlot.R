91234572a80eedc479e737c956b7c9a0686f9e48
sanchayana2007 <sanchayana2007@gmail.com>
2019-02-18 09:06:42 +0530

    plots

--- DA/R_DA/BarPlot.R
+++ DA/R_DA/BarPlot.R
@@ -8,5 +8,8 @@ head(df)
 
 avgprice=c(mean(df[which(df$name=='Barton LLC'),]$ext.price),mean(df[which(df$name=='Kulas Inc'),]$ext.price))
 range(avgprice)
-barplot(avgprice ,xlab = "Cust name",ylab = "Avg",ylim = c(0,1400))
+barplot(avgprice ,xlab = "Cust name",ylab = "Avg",ylim = c(0,1400),col=c("darkblue","red"),legend = rownames(avgprice), beside=TRUE)
+t=c("Barton LLC","Kulas Inc")
+# Simple Horizontal Bar Plot with Added Labels
+
 
