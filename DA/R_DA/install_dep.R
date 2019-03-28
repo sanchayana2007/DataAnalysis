3de3a1d5215861501d69879a13ff17b4f5bec3ac
san <sanchayana2007@gmail.com>
2019-03-28 07:11:06 +0530

    Folder restructurd

--- DA/R_DA/install_dep.R
+++
@@ -1,2 +0,0 @@
-install.packages(c("xlsx","rminer","MASS","class","e1071","rpart","rpart.plot","devtools","caret","arules","arulesviz","cluster","xts",
-"matrixcalc","forecast","igraph","treemap","rworldmap","ggmap"),dependencies = T )
